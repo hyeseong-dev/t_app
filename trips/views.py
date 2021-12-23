b8b8b07751bd3846a647057ed67f311eced85518
이혜성 <hyeseong43@gmail.com>
2021-12-24 00:14:05 +0900

    1st initial commit

---
+++ trips/views.py
@@ -0,0 +1,9 @@
+from django.contrib.auth import get_user_model
+from rest_framework import generics
+
+from .serializers import UserSerializer
+
+
+class SignUpView(generics.CreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
